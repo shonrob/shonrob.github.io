1ef3dc7fd25a08e6e7aed556a5e8b42ffd213fc1
shonrob <rob19063@byui.edu>
2020-05-20 10:36:41 -0400

    Update js lesson 4 date

--- lesson4/javascript/currentdate.js
+++ lesson4/javascript/currentdate.js
@@ -1,3 +1,33 @@
-var fulldate = new Date(document.lastModified);
+const options = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
+document.getElementById("currentDate").innerHTML = new Date().toLocaleDateString("en-US", options);
 
-document.getElementById("time").textContent = fulldate;
+/* Another way to show date- Much longer of a process though.
+
+const dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+const months= [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "Novemeber",
+    "December"
+];
+const todaysDate = new Date();
+const dayName = dayNames[todaysDate.getDay()];
+const monthName = months[todaysDate.getMonth()];
+const currentDate = dayName + ", " + todaysDate.getDate() + " " + monthName + ", " + todaysDate.getFullYear();
+document.getElementById("currentDate").textContent = currentDate;  /*
