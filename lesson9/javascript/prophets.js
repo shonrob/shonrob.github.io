271245c201c00805e4590bd9c6f3310e292e0ae2
shonrob <rob19063@byui.edu>
2020-06-17 17:43:43 -0400

    update

--- lesson9/javascript/prophets.js
+++ lesson9/javascript/prophets.js
@@ -10,7 +10,7 @@ fetch(requestURL)
 
     const prophets = jsonObject['prophets']; 
 
-
+      // making a variable for each element
   for (let i = 0; i < prophets.length; i++ ) {
     let card = document.createElement('section');
     let fullName = document.createElement('h2');
@@ -19,14 +19,14 @@ fetch(requestURL)
     let image = document.createElement('img');
     let alt = document.createElement('alt');
 
+      // calling each variable into the JSON Javascript
     fullName.textContent = prophets[i].name + ' ' + prophets[i].lastname;
     alt.setAttribute('alt', prophets[i].name + prophets[i].lastname + ' - ' + prophets[i].order);
     image.setAttribute('src', prophets[i].imageurl);
-
     bDay.textContent = "Date of Birth: " +prophets[i].birthdate;
     birth.textContent = "Place of Birth: " +prophets[i].birthplace;
 
-    
+      // previewing each element with its acquired information to show on the site
     card.appendChild(fullName);
     card.appendChild(alt);
     card.appendChild(bDay);
