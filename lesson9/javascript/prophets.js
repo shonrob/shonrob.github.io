9017f883e2c80f8414e408b412fbf4897d8d4a60
shonrob <rob19063@byui.edu>
2020-07-08 20:48:35 -0400

    update lesson 12 folder

--- lesson9/javascript/prophets.js
+++ lesson9/javascript/prophets.js
@@ -6,7 +6,7 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
 
-    // console.table(jsonObject);  // temporary checking for valid response and data parsing
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
 
     const prophets = jsonObject['prophets']; 
 
